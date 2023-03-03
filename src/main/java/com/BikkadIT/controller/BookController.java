33a3780e10ddf9de046b79ff71948a5275efd6c7
KarunTiple <123485211+KarunTiple@users.noreply.github.com>
2023-03-03 15:22:21 +0530

    Update BookController.java

--- src/main/java/com/BikkadIT/controller/BookController.java
+++ src/main/java/com/BikkadIT/controller/BookController.java
@@ -3,13 +3,13 @@ package com.BikkadIT.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.BikkadIT.model.Book;
 
-@Controller
+@RestController
 public class BookController {
 
 	@GetMapping("/books")
